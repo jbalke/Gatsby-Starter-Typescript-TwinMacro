018ec33819cadda52b58bced8eb81700577bfcb1
John Balke <john.balke@gmail.com>
2020-12-02 10:04:43 +0000

    Stylelint configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,19 @@
 module.exports = {
   plugins: [
-    require("tailwindcss")("./tailwind.config.js"),
-    require("postcss-flexbugs-fixes"),
-    require("postcss-import"),
-    require("postcss-preset-env")({
+    require('postcss-import')({
+      plugins: [require('stylelint')],
+    }),
+    require('tailwindcss')('./tailwind.config.js'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
       autoprefixer: {
-        flexbox: "no-2009",
+        flexbox: 'no-2009',
       },
       stage: 3,
       features: {
-        "custom-properties": false,
+        'custom-properties': false,
       },
-      browsers: ["> 1%", "last 2 versions", "Firefox ESR"],
+      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'],
     }),
   ],
-}
+};
